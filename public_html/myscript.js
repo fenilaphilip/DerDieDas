3e419f45876aa44be9ea13a584c74e83808d1d3c
fenila <100304937+fenilaphilip@users.noreply.github.com>
2022-04-07 14:54:22 +0200

    Loading categories dynamically

--- public_html/myscript.js
+++ public_html/myscript.js
@@ -1,2 +1,25 @@
 
 
+$(document).ready(function () {
+
+    $.get("data/categories.json", function (data, status) {
+        console.log(data);
+        for (i = 0; i < data.length; i++) {
+            console.log(data[i]);
+            let category = "<li "
+                    + "class=\"pointer\" "
+                    + "onclick= \"foropen('" + data[i] + "')\" >"
+                    + data[i] + "</li>";
+            $("#categories-container").append(category);
+        }
+    });
+});
+
+function foropen(category) {
+    $("#play-ground").load("data/" + category + ".json");
+
+}
+
+$("buttonxx").click(function () {
+
+});
