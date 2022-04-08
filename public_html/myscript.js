eb61916ef3fb67531df35e82b8c7e3502fdb62df
fenila <100304937+fenilaphilip@users.noreply.github.com>
2022-04-08 18:28:42 +0200

    Category data is processed correctly.
    * Data is put in a variable.
    * Displayed first word by default.

--- public_html/myscript.js
+++ public_html/myscript.js
@@ -1,25 +1,31 @@
 
 
 $(document).ready(function () {
-
+    $("#game-area").hide();
     $.get("data/categories.json", function (data, status) {
         console.log(data);
         for (i = 0; i < data.length; i++) {
             console.log(data[i]);
             let category = "<li "
                     + "class=\"pointer\" "
-                    + "onclick= \"foropen('" + data[i] + "')\" >"
+                    + "onclick= \"on_category_clicked('" + data[i] + "')\" >"
                     + data[i] + "</li>";
             $("#categories-container").append(category);
         }
     });
 });
 
+let cat_data;
 
-function foropen(category) {
-    $.get("data/" + category + ".json",function(data,status){
-        console.log(data);
-        $("#openCategoryName").html(data.article +" "+ data.category);  
+function on_category_clicked(category) {
+    $.get("data/" + category + ".json", function (data, status) {
+        cat_data = data;
+        $("#openCategoryName").html(data.article + " " + data.category);
+        on_next_button_clicked();
+        $("#game-area").show();
     });
 
+}
+function on_next_button_clicked() {
+    $("#play-ground").html(cat_data.words[0].nom);
 }
